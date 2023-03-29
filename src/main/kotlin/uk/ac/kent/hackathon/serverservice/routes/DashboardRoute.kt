c6cac4875176e1d736399f93a8b405086ed2e342
Ben Martin <ben@brmartin.co.uk>
2023-03-29 15:51:12 +0100

    Add link to Etherscan per token

--- src/main/kotlin/uk/ac/kent/hackathon/serverservice/routes/DashboardRoute.kt
+++ src/main/kotlin/uk/ac/kent/hackathon/serverservice/routes/DashboardRoute.kt
@@ -10,6 +10,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.router.Route
 import com.vaadin.flow.spring.security.AuthenticationContext
 import jakarta.annotation.security.PermitAll
+import uk.ac.kent.hackathon.serverservice.config.ContractConfig
 import uk.ac.kent.hackathon.serverservice.domain.Token
 import uk.ac.kent.hackathon.serverservice.entities.UserDetailsImpl
 import uk.ac.kent.hackathon.serverservice.layouts.MainLayout
@@ -17,7 +18,7 @@ import uk.ac.kent.hackathon.serverservice.services.TokenService
 
 @Route("dashboard", layout = MainLayout::class)
 @PermitAll
-class DashboardRoute(tokenService: TokenService, authenticationContext: AuthenticationContext) : VerticalLayout() {
+class DashboardRoute(tokenService: TokenService, authenticationContext: AuthenticationContext, contractConfig: ContractConfig) : VerticalLayout() {
     init {
         val userDetailsImpl = authenticationContext.getAuthenticatedUser(UserDetailsImpl::class.java).get()
 
@@ -25,7 +26,7 @@ class DashboardRoute(tokenService: TokenService, authenticationContext: Authenti
         val etherscanLink = Anchor("https://sepolia.etherscan.io/address/${userDetailsImpl.etherAccount.ethPkHash}", userDetailsImpl.etherAccount.ethPkHash)
         val nftHeading = H3("My NFTs")
 
-        val div = Div(title, etherscanLink, nftHeading, grid(tokenService, userDetailsImpl)).apply {
+        val div = Div(title, etherscanLink, nftHeading, grid(tokenService, userDetailsImpl, contractConfig)).apply {
             maxWidth = "800px"
             setWidthFull()
         }
@@ -35,11 +36,13 @@ class DashboardRoute(tokenService: TokenService, authenticationContext: Authenti
 
     private fun grid(
         tokenService: TokenService,
-        userDetailsImpl: UserDetailsImpl
+        userDetailsImpl: UserDetailsImpl,
+        contractConfig: ContractConfig
     ): Grid<Token> {
         val tokensGrid = Grid(Token::class.java, false)
         tokensGrid.addColumn(Token::tokenId).setHeader("ID")
         tokensGrid.addColumn(Token::description).setHeader("Description")
+        tokensGrid.addComponentColumn { Anchor("https://sepolia.etherscan.io/nft/${contractConfig.address}/${it.tokenId}", "Details") }.setHeader("Description")
         tokensGrid.setItems(tokenService.getTokensByUser(userDetailsImpl))
         return tokensGrid
     }
