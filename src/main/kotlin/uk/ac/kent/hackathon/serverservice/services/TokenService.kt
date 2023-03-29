e4b7781add51c8a3aaaa00f63becf098db3b5842
Ben Martin <ben@brmartin.co.uk>
2023-03-29 01:48:00 +0100

    Utilise accumulation function over NFT tokens

--- src/main/kotlin/uk/ac/kent/hackathon/serverservice/services/TokenService.kt
+++ src/main/kotlin/uk/ac/kent/hackathon/serverservice/services/TokenService.kt
@@ -3,7 +3,7 @@ package uk.ac.kent.hackathon.serverservice.services
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForObject
-import org.springframework.web.util.UriComponentsBuilder
+import org.springframework.web.util.UriComponentsBuilder.fromHttpUrl
 import uk.ac.kent.hackathon.serverservice.config.EtherscanConfig
 import uk.ac.kent.hackathon.serverservice.domain.Token
 import uk.ac.kent.hackathon.serverservice.domain.TokenNFTTxResponse
@@ -13,7 +13,7 @@ import uk.ac.kent.hackathon.serverservice.entities.UserDetailsImpl
 class TokenService(private val etherscanConfig: EtherscanConfig, private val restTemplate: RestTemplate) {
 
     fun getTokensByUser(userDetailsImpl: UserDetailsImpl): Collection<Token> {
-        val uri = UriComponentsBuilder.fromHttpUrl("https://api-sepolia.etherscan.io/api")
+        val uri = fromHttpUrl("https://api-sepolia.etherscan.io/api")
             .queryParam("module", "account")
             .queryParam("action", "tokennfttx")
             .queryParam("contractaddress", "0x05d1AD3ff7bed18e442AE9AcB34d967a1638dC71")
@@ -26,16 +26,14 @@ class TokenService(private val etherscanConfig: EtherscanConfig, private val res
             .queryParam("apikey", etherscanConfig.etherscanApiKey)
             .build().toUri()
 
-        val newList = mutableListOf<Token>()
-        restTemplate.getForObject<TokenNFTTxResponse>(uri).result.forEach {
-            val token = Token(it.tokenId, userDetailsImpl, "") // TODO: Fix description lol
-            if (it.to == userDetailsImpl.etherAccount.ethPkHash) {
-                newList.add(token)
-            } else if (it.from == userDetailsImpl.etherAccount.ethPkHash) {
-                newList.remove(token)
+        return restTemplate.getForObject<TokenNFTTxResponse>(uri).result.fold(mutableListOf()) { acc, nftResponse ->
+            val token = Token(nftResponse.tokenId, userDetailsImpl, "") // TODO: Fix description lol
+            acc.apply {
+                when (userDetailsImpl.etherAccount.ethPkHash) {
+                    nftResponse.to -> add(token)
+                    nftResponse.from -> remove(token)
+                }
             }
         }
-
-        return newList
     }
 }
